8cce62058437be9100750acd27bbf84a75044256
YashChhadva <yashchhadva@gmail.com>
2020-08-10 18:36:27 +0530

    fifth commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,25 +22,7 @@ exports.signUp = (req, res) => {
     });
 };
 
-// using async/await
-// exports.signup = async (req, res) => {
-//     try {
-//         const user = await new User(req.body);
-//         console.log(req.body);
 
-//         await user.save((err, user) => {
-//             if (err) {
-//                 // return res.status(400).json({ err });
-//                 return res.status(400).json({
-//                     error: 'Email is taken'
-//                 });
-//             }
-//             res.status(200).json({ user });
-//         });
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// };
 
 exports.signIn = (req, res) => {
     // find the user based on email
