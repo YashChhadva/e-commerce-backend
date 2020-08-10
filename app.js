3dc77d3d8376d7576daa87388f6abc338ffaca6d
YashChhadva <yashchhadva@gmail.com>
2020-08-10 17:01:11 +0530

    first commit

--- app.js
+++ app.js
@@ -90,6 +90,10 @@ app.use('/api', braintreeRoutes);
 
 const port = process.env.PORT || 8000;
 
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('../client/ecommerce/build'));
+// }
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
